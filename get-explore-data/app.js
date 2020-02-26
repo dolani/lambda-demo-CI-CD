c5208fd5c67723da2fdfe970bb56759ee88d1f91
Ayoola Akindolani <aakindolani@eminenttechnology.com>
2020-02-26 19:07:09 +0100

    add cors

--- get-explore-data/app.js
+++ get-explore-data/app.js
@@ -8,8 +8,8 @@ function createResponse(statusCode, message) {
     return {
         statusCode: statusCode,
         headers: {
-            // "Access-Control-Allow-Origin": "*", // Required for CORS support to work
-            // "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS 
+            "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+            "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS 
         },
         body: JSON.stringify(message)
     };
